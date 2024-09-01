93af7d5560589fed24673b3de45720636f34a9e1
egorgavrilov <Egor4it@yandex.ru>
2024-09-01 21:42:32 +0500

    Конечный скрипт запуска

--- scrapers/async_parsers/async_parser_main.py
+++ scrapers/async_parsers/async_parser_main.py
@@ -6,9 +6,11 @@ from async_parser import AsyncParser
 async def main():
     parser = AsyncParser(base_url='https://parsinger.ru')
     pages = await parser.get_pages_href(url='index2_page_1.html')
-    page = pages[0]
-    cards = await parser.get_items_cards(page)
+    cards_list = await asyncio.gather(*[parser.get_items_cards(url=page) for page in pages])
+    items_list = await asyncio.gather(*[parser.get_information_about_item_from_card(card)
+                                        for chunk_cards in cards_list for card in chunk_cards])
+    parser.save_information_about_items(items_list)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
-
